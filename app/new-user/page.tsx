5903e2f8f16a0142c15fbd3ebd7e72c05c7507f8
Yakin Teshome <yakin16tm@gmail.com>
2023-10-26 13:16:32 +0300

    feat: sync clerk auth with db

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,4 +1,36 @@
-export default function NewUser() {
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+
+const CreateNewUser = async () => {
+    const user = await currentUser()
+
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user?.id as string
+        }
+    })
+    
+
+    if (!match) {
+        await prisma.user.create({
+            data: {
+                clerkId: user?.id,
+                email: user?.emailAddresses[0].emailAddress,
+            }
+        })
+
+    }
+
+    redirect('/journal')
+}
+
+export default async function NewUser() {
+
+    await CreateNewUser()
+
     return (
-        <div>New user</div>)
+        <div>...Loading</div>)
+
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+
+  const href = userId ? '/journal': '/new-user'
+
   return (
     <main className="w-screen h-screen bg-black text-white  flex justify-center items-center">
       <div className='w-full max-w-[800px] mx-auto' >
         <h1 className='text-5xl mb-5'>The best journal app.</h1>
         <p className='text-3xl text-white mb-4'>Write your thoughts and ideas in a beautiful and simple way.</p>
         <div className=''>
-          <Link href='/journal'>
+          <Link href={href}>
             <button className='bg-blue-500 text-white px-4 py-2 rounded-lg mt-8 text-xl capitalize'>Get started</button>
           </Link>
         </div>
