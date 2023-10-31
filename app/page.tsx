bbb98703813804b918c995b744895ef8e0e6adba
Yakin Teshome <yakin16tm@gmail.com>
2023-10-31 10:50:10 +0300

    fix: text content modification

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,9 @@ export default async function Home() {
   return (
     <main className="w-screen h-screen bg-black text-white  flex justify-center items-center">
       <div className='w-full max-w-[800px] mx-auto' >
-        <h1 className='text-5xl mb-5'>The best journal app.</h1>
-        <p className='text-3xl text-white mb-4'>Write your thoughts and ideas in a beautiful and simple way.</p>
+        <h1 className='text-5xl mb-5'>The best journal app, Period.</h1>
+        <p className='text-3xl text-white mb-4'>This is the best app for tracking your mood through out your life. All
+          you have to do is be honest.</p>
         <div className=''>
           <Link href={href}>
             <button className='bg-blue-500 text-white px-4 py-2 rounded-lg mt-8 text-xl capitalize'>Get started</button>
