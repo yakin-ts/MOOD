d059a0a4ba313b222daa9c21d9e9feeb8f4d4358
Yakin Teshome <yakin16tm@gmail.com>
2023-10-31 13:22:24 +0300

    fix typo and cleanup

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,8 +1,9 @@
 import JournalEditor from "@/components/JournalEditor";
 import getUserByClerkId from "@/utils/auth";
 import { prisma } from "@/utils/db";
+import { FunctionComponent } from "react";
 
-const getEntry = async (journalId) => {
+const getEntry = async (journalId: string) => {
     const user = await getUserByClerkId()
 
     const entry = await prisma.journalEntry.findUnique({
@@ -21,15 +22,19 @@ const getEntry = async (journalId) => {
     return entry
 }
 
-const JournalEditorPage = async ({ params }) => {
+interface Props {
+    params: { id: string }
+}
+
+const JournalEditorPage: FunctionComponent<Props> = async ({ params }) => {
     const entry = await getEntry(params.id)
-    
+
     return (
         <div className='grid grid-cols-3'>
-                <JournalEditor entry={entry} />
+            <JournalEditor entry={entry} />
         </div>
-        )
-            
+    )
+
 }
 
 export default JournalEditorPage;
--- components/Question.tsx
+++ components/Question.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import { useState } from "react"
+import React, { useState } from "react"
+import { askQuestion } from "@/utils/api"
 
 const Question = () => {
     const [value, setValue] = useState('')
-    const [loading, setLoading] = useSatate(False)
+    const [loading, setLoading] = useState(false)
     const [answer, setAnswer] = useState('')
 
-    cons handleSubmit = async (e) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setLoading(true)
         const answer = await askQuestion(value)
@@ -20,15 +21,15 @@ const Question = () => {
         <div>
             <form onSubmit={handleSubmit}>
                 <input disabled={loading} value={value} type='text'
-                    onChange={() => etValue(e.target.value)} placeholder="Ask a Question"
+                    onChange={(e) => setValue(e.target.value)} placeholder="Ask a Question"
                     className='border-black/120 text-lg'
                 />
-                <button disabled={loading} type='submit' 
-                className='px-4 py-2 rounded-lg text-white text-lg bg-zinc-600/20'>Ask</button>
+                <button disabled={loading} type='submit'
+                    className='px-4 py-2 rounded-lg text-white text-lg bg-zinc-600/20'>Ask</button>
             </form>
             {loading && <p className='text-lg text-gray-500'>Thinking...</p>}
             {answer && <p className='text-lg text-gray-500'>{answer}</p>}
-            
+
         </div>
     )
 }
